7cce2e6a5aa63406b6b5d932605bd2c35b0258aa
Christine Ramos <tineramos.ph@gmail.com>
2022-06-22 15:11:28 +0100

    Initial Commit

---
+++ AnimatableData/AnimatableDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatableDataApp.swift
+//  AnimatableData
+//
+//  Created by Tine Ramos on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatableDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
